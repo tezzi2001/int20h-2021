51e47e5799312ec66ca011aad8a2e36b13fee16a
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-02-21 00:00:59 +0200

    fix upload img 5

--- src/main/java/com/bondarenko/int20h2021/controller/AdvertisementController.java
+++ src/main/java/com/bondarenko/int20h2021/controller/AdvertisementController.java
@@ -19,8 +19,8 @@ public class AdvertisementController {
     private final AdvertisementService advertisementService;
 
     @PostMapping("/createAdvertisementLost")
-    public void createAdvertisementLost(@RequestParam("advertisement") String advertisement, @RequestParam("photo") MultipartFile photo, HttpServletRequest request) {
-        System.out.println("advertisement " + advertisement);
+    public void createAdvertisementLost(@RequestParam("advertisement") AdvertisementDto advertisementDto, @RequestParam("photo") MultipartFile photo, HttpServletRequest request) {
+        System.out.println("advertisement " + advertisementDto.toString());
         System.out.println("photo " + photo.getOriginalFilename());
 //        advertisementService.createAdvertisementLost(advertisementDto, photo, getAuthorizationHeader(request));
     }
