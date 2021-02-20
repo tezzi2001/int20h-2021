25b2bc5a5c4995c44af9c6e20815c18b15afd6c1
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-02-20 21:07:03 +0200

    fix CORS 6

--- src/main/java/com/bondarenko/int20h2021/filter/AddCorsResponseHeaderFilter.java
+++ src/main/java/com/bondarenko/int20h2021/filter/AddCorsResponseHeaderFilter.java
@@ -10,7 +10,10 @@ public class AddCorsResponseHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(request, response);
     }
 }
