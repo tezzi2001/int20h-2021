92fd66ce6422b9f5198fdfd33a45c2ef6fa415b2
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-02-21 07:03:30 +0200

    fix CORS

--- src/main/java/com/bondarenko/int20h2021/filter/AddCorsResponseHeaderFilter.java
+++ src/main/java/com/bondarenko/int20h2021/filter/AddCorsResponseHeaderFilter.java
@@ -11,7 +11,7 @@ public class AddCorsResponseHeaderFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8080, https://petsavior.herokuapp.com");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
